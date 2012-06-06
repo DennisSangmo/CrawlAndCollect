eb6e0ece25d5fca5ec60f15e2f1742ff30b087fc
Dennis.Sangmo <dennis.sangmo@gmail.com>
2012-06-06 23:38:38 +0200

    Linkvalidator refactoring, easyer to test

--- src/CrawlAndCollect/CrawlAndCollect.LinkValidatorEndPoint/LinkValidatorEndPoint.cs
+++ src/CrawlAndCollect/CrawlAndCollect.LinkValidatorEndPoint/LinkValidatorEndPoint.cs
@@ -1,13 +1,9 @@
 ﻿using System;
-using System.Linq;
-using CrawlAndCollect.Core.Entities.BlockUri;
-using CrawlAndCollect.Core.Entities.CrawledUri;
+using CrawlAndCollect.Core.Bs;
 using CrawlAndCollect.Core.Entities.Link;
 using CrawlAndCollect.Core.Entities.Log;
-using CrawlAndCollect.Core.Extensions;
 using CrawlAndCollect.Core.NserviceBus.Messages;
 using CrawlAndCollect.Core.Persistence.RavenDB;
-using CrawlAndCollect.Core.Persistence.RavenDB.Indexes;
 using NServiceBus;
 using Raven.Client;
 
@@ -16,18 +12,18 @@ namespace CrawlAndCollect.LinkValidatorEndPoint {
     {
         private readonly Guid _resourceManagerId = Guid.Parse("0201c8f3-8ffc-4bda-a45d-de54cfc02316");
         private readonly IDocumentSession _session;
+        private readonly LinkValidator _validator;
         public IBus Bus { get; set; }
-
-        public string[] ValidTopDomains = new[] { "se", "nu" };
 
         public LinkValidatorEndPoint() {
             _session = DocumentStoreFactory.CreateEndPointStore(_resourceManagerId).OpenSession();
+            _validator = new LinkValidator(_session);
         }
 
         public void Handle(ValidateLinkMessage message) {
             _session.Store(new Link(message.PageUrl, message.Text, message.Href, message.NoFollow));
 
-            if (ValidateUri(message.Href))
+            if (_validator.Validate(message.Href))
                 Bus.Send(new CrawlUrlMessage(message.Href));
 
             _session.SaveChanges();
@@ -35,7 +31,8 @@ namespace CrawlAndCollect.LinkValidatorEndPoint {
 
         public void Handle(RequestUriCrawlMessage message)
         {
-            if(ValidateUri(message.Uri)) {
+            if (_validator.Validate(message.Uri))
+            {
                 _session.Store(new LogRow(DateTime.Now, LogLevel.Information, LogSender.LinkValidatorEndpoint, string.Format("Requested uri accepted ({0})", message.Uri), "The requested uri has been accepted and sent to the crawler!"));
                 Bus.Send(new CrawlUrlMessage(message.Uri));
             } else {
@@ -43,40 +40,6 @@ namespace CrawlAndCollect.LinkValidatorEndPoint {
             }
 
             _session.SaveChanges();
-        }
-
-        private bool ValidateUri(Uri uri) {
-
-            // Is an swedish site?
-            if (!IsCrawlableTopDomain(uri))
-                return false;
-
-            // Is in blocklists?
-            if (IsPageBlocked(uri))
-                return false;
-
-            // Already crawled this url?
-            if (IsPageCrawled(uri))
-                return false;
-
-            return true;
-        }
-
-        private bool IsPageCrawled(Uri uri)
-        {
-            return _session.Query<CrawledUri, CrawledUriIndex>().Any(x => x.Uri == uri);
-        }
-
-        private bool IsCrawlableTopDomain(Uri uri)
-        {
-            return ValidTopDomains.Any(validTopDomain => uri.Host.EndsWith(validTopDomain));
-        }
-
-        private bool IsPageBlocked(Uri uri)
-        {
-            var domain = uri.DomainAndTopDomainWitoutWww();
-            return _session.Query<BlockUri, BlockedUriIndex>().Any(x => x.Uri == uri) ||
-                   _session.Query<BlockDomain, BlockedDomainIndex>().Any(x => x.Domain == domain);
         }
     }
 }
